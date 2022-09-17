4de666c2d0e0c68993a45ae03946c15665a85490
bosley <bosley117@gmail.com>
2022-09-17 22:26:41 +0000

    loops and created test_scripts

--- sauros/processor/processor_std_builtins.cpp
+++ sauros/processor/processor_std_builtins.cpp
@@ -4,6 +4,40 @@
 
 namespace sauros {
 
+namespace {
+
+static inline bool eval_truthy(cell_c &cell, location_s &location) {
+   switch (cell.type) {
+   case cell_type_e::STRING:
+      return (!cell.data.empty());
+      break;
+   case cell_type_e::LIST:
+      return (!cell.list.empty());
+      break;
+   case cell_type_e::LAMBDA:
+      return true;
+      break;
+   case cell_type_e::DOUBLE:
+      [[fallthrough]];
+   case cell_type_e::INTEGER: {
+      try {
+         auto v = std::stod(cell.data);
+         return (v > 0.0);
+      } catch (const std::invalid_argument &) {
+         throw processor_c::runtime_exception_c("Invalid data type given for operand",
+                                 location);
+      } catch (const std::out_of_range &) {
+         throw processor_c::runtime_exception_c("Item caused out of range exception",
+                                 location);
+      }
+      break;
+   }
+   }
+   return false;
+}
+
+}
+
 void processor_c::populate_standard_builtins() {
 
    // Populate symbols that users are not allowed to redefine
@@ -35,6 +69,7 @@ void processor_c::populate_standard_builtins() {
    _key_symbols.insert("seq");
    _key_symbols.insert("sneq");
    _key_symbols.insert("assert");
+   _key_symbols.insert("loop");
 
    _builtins["exit"] = cell_c(
        [this](std::vector<cell_c> &cells,
@@ -190,11 +225,11 @@ void processor_c::populate_standard_builtins() {
             }
 
             if (cell_type_e::STRING == (*result).type && (*result).data.empty()) {
-               throw assertion_exception_c(cells[1].data, (*c).location);
+               throw assertion_exception_c("assertion failure: " + cells[1].data, (*c).location);
             } else if (cell_type_e::INTEGER == (*result).type && std::stoull((*result).data) < 1) {
-               throw assertion_exception_c(cells[1].data, (*c).location);
+               throw assertion_exception_c("assertion failure: " + cells[1].data, (*c).location);
             } else if (cell_type_e::DOUBLE == (*result).type && std::stod((*result).data) <= 0.0) {
-               throw assertion_exception_c(cells[1].data, (*c).location);
+               throw assertion_exception_c("assertion failure: " + cells[1].data, (*c).location);
             }
          }
          return {CELL_TRUE};
@@ -281,6 +316,54 @@ void processor_c::populate_standard_builtins() {
           return {lambda};
        });
 
+   _builtins["loop"] = cell_c(
+       [this](std::vector<cell_c> &cells,
+              std::shared_ptr<environment_c> env) -> std::optional<cell_c> {
+          if (cells.size() != 3 && cells.size() != 4) {
+             throw runtime_exception_c("loop command expects 3 parameters, but" +
+                                           std::to_string(cells.size() - 1) +
+                                           " were given",
+                                       cells[0].location);
+          }
+
+          auto &conditional_cell = cells[1];
+
+          cell_c* body{nullptr};
+          cell_c* post{nullptr};
+
+          if (cells.size() == 3) {
+            body = &cells[2];
+          }
+          else if (cells.size() == 4) {
+            post = &cells[2];
+            body = &cells[3];
+          }
+
+          while (1) {
+
+            auto loop_environment = std::make_shared<environment_c> (env);
+
+            // Check the condition
+            auto conditional_result = process_cell(conditional_cell, loop_environment);
+            if (! conditional_result.has_value()) {
+               break;
+            }
+
+            if (!eval_truthy((*conditional_result), cells[0].location)) {
+               break;
+            }
+
+            // Execute the body
+            process_cell(*body, loop_environment);
+
+            if (post) {
+               process_cell(*post, loop_environment);
+            }
+          }
+
+          return {};
+       });
+
    _builtins["block"] = cell_c(
        [this](std::vector<cell_c> &cells,
               std::shared_ptr<environment_c> env) -> std::optional<cell_c> {
@@ -305,8 +388,7 @@ void processor_c::populate_standard_builtins() {
           auto &variable_name = cells[1].data;
 
           // If this isn't found it will throw :)
-          { auto _ = env->find(variable_name); }
-
+          auto containing_env = env->find(variable_name);
           auto value = process_cell(cells[2], env);
 
           if ((*value).type == cell_type_e::SYMBOL) {
@@ -319,8 +401,8 @@ void processor_c::populate_standard_builtins() {
                                        cells[2].location);
           }
 
-          env->set(variable_name, (*value));
-          return {env->get(variable_name)};
+          containing_env->set(variable_name, (*value));
+          return {containing_env->get(variable_name)};
        });
 
    _builtins["list"] = cell_c(
@@ -371,35 +453,7 @@ void processor_c::populate_standard_builtins() {
                  cells[1].location);
           }
 
-          bool is_true{false};
-          switch ((*eval).type) {
-          case cell_type_e::STRING:
-             is_true = (!(*eval).data.empty());
-             break;
-          case cell_type_e::LIST:
-             is_true = (!(*eval).list.empty());
-             break;
-          case cell_type_e::LAMBDA:
-             is_true = true;
-             break;
-          case cell_type_e::DOUBLE:
-             [[fallthrough]];
-          case cell_type_e::INTEGER: {
-             try {
-                auto v = std::stod((*eval).data);
-                is_true = (v > 0.0);
-             } catch (const std::invalid_argument &) {
-                throw runtime_exception_c("Invalid data type given for operand",
-                                          cells[0].location);
-             } catch (const std::out_of_range &) {
-                throw runtime_exception_c("Item caused out of range exception",
-                                          cells[0].location);
-             }
-             break;
-          }
-          }
-
-          if (is_true) {
+          if (eval_truthy((*eval), cells[0].location)) {
             return process_cell(cells[2], env);
           } else if (cells.size() == 4) {
             return process_cell(cells[3], env);
---
+++ test_scipts/run.py
@@ -0,0 +1,102 @@
+import re
+import glob
+import sys
+import os
+import subprocess
+import threading
+import time
+
+if len(sys.argv) < 2:
+   print("Expected path to sauros binary")
+   exit(1)
+
+binary = sys.argv[1]
+
+print("\n<< SAUROS TESTS >>\n")
+check_directory = os.getcwd()
+print("CWD : ", check_directory)
+
+test_directories = [
+  check_directory + "/tests"
+]
+
+def time_to_ms_str(t):
+   return str(round(t * 1000, 4)) + "ms"
+
+def display_result(result_item):
+   out = "-" * 10
+   out += "\n\n"
+   out += "Test File    : " + result_item["name"] + "\n"
+   out += "Parser    : "
+
+   if not result_item["result"]["success"]:
+      out += "[FAILED] after " + time_to_ms_str(result_item["result"]["time"]) + "\n"
+      out += "\n"
+      out += "\t---- output ----\n"
+      out += result_item["result"]["output"]
+      out += "\t----------------\n"
+      print(out)
+      exit(1)
+   else:
+      out += "[PASSED] after " + time_to_ms_str(result_item["result"]["time"]) + "\n"
+
+   print(out)
+
+def test_item(id, expected_result, item):
+   results = {}
+   start = time.time()
+   result = subprocess.run([binary, item], stdout=subprocess.PIPE)
+   end = time.time()
+   parser_status = True
+
+   decoded = result.stdout.decode("utf-8")
+
+   results["name"] = item
+
+   results["result"] = {
+   "time": end - start,
+   "success": result.returncode == int(expected_result),
+   "output": decoded
+   }
+   return results
+
+def retrieve_objects_from(directory):
+   os.chdir(directory)
+   items_in_dir = glob.glob("*.sau")
+   results = []
+   for item in items_in_dir:
+      result = {}
+      result["expected_code"] = item.split("_")[0]
+      result["path"] = directory + "/" + item
+      results.append(result)
+   return results
+
+def build_exec_list(dirs):
+   exec_list = []
+   for dir in dirs:
+      print("Scanning directory : ", dir)
+      exec_list.append(retrieve_objects_from(dir))
+   print("")
+   return exec_list
+
+def task(id, jobs):
+   results = []
+   for item in jobs:
+      results.append(test_item(id, item["expected_code"], item["path"]))
+   for item in results:
+      display_result(item)
+
+def linear_run():
+   exec_list = build_exec_list(test_directories)
+   os.chdir(check_directory)
+   for item in exec_list:
+      task(0, item)
+
+run_time_start = time.time()
+linear_run()
+run_time_end = time.time()
+
+print("-" * 10)
+print("\nChecks complete after ", round(run_time_end - run_time_start, 4), " seconds\n")
+
+exit(0)
