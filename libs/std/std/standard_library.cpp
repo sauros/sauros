a0de288247a0a1d28c8e8fdaae7dfcb9ddddcede
bosley <bosley117@gmail.com>
2022-09-20 01:52:13 +0000

    added lib stuff

---
+++ libs/std/std/standard_library.cpp
@@ -0,0 +1,10 @@
+#include "standard_library.hpp"
+
+#include <iostream>
+
+std::optional<sauros::cell_c> print_cell
+   (std::vector<sauros::cell_c> &cells, std::shared_ptr<sauros::environment_c> env) {
+
+      std::cout << "I do nothing yet" << std::endl;
+      return {};
+}
