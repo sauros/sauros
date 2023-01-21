4314ac47b4e959bc5fcba88dff49f667ad4069fe
bosley <bosley117@gmail.com>
2023-01-21 15:42:37 -0500

    made void cell, fleshed out and exampled fileio

---
+++ pkgs/file_io/file_io.hpp
@@ -0,0 +1,22 @@
+#ifndef SAUROS_PKG_STD_FMT
+#define SAUROS_PKG_STD_FMT
+
+#include <sauros/sauros.hpp>
+
+#ifdef WIN32
+#define API_EXPORT __declspec(dllexport)
+#else
+#define API_EXPORT
+#endif
+
+extern "C" {
+
+/*
+   Encode cells into a string
+*/
+API_EXPORT
+extern sauros::cell_ptr
+_pkg_file_io_get_handle_(sauros::cells_t &cells,
+                         std::shared_ptr<sauros::environment_c> env);
+}
+#endif
