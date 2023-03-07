00bf085dc79c34bb70ec85cbd6f988584abd50cd
bosley <bosley117@gmail.com>
2023-03-07 18:01:57 -0500

    crazy

--- src/libsauros/tests/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "CppUTest/CommandLineTestRunner.h"
-
-int main(int ac, char **av) {
-  MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
-  return CommandLineTestRunner::RunAllTests(ac, av);
-}
--- vscode-extension/install.sh
+++
@@ -1,2 +0,0 @@
-#cp -r sauros ~/.vscode-server/extensions/sauros-0.0.1
-cp -r sauros ~/.vscode/extensions/sauros-0.0.1
