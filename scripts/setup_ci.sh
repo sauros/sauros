51d049f823ce26bd967a7655529c077be5c27acc
bosley <bosley117@gmail.com>
2023-03-07 19:15:12 -0500

    submodules

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -16,5 +16,3 @@ tar -xzvf /tmp/cpputest.tar.gz -C /tmp
 if [ "$CC" = "clang" ]; then export CXX="clang++"; fi && cd /tmp/cpputest-* && ./configure --prefix=/usr/local && make && sudo make install
 
 cd /tmp/cpputest-* && ./configure --prefix=/usr/local && make && sudo make install
-
-git submodule update --init --recursive
