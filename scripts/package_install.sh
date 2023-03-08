30995daa9225adc23448807353c578d98f9da0fe
bosley <bosley117@gmail.com>
2023-03-07 20:28:00 -0500

    more tests

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -1,2 +1,2 @@
 
-git clone https://github.com/sauros/packages.git && cd packages && python3 install.py
+git clone https://github.com/sauros/packages.git && cd packages && export SAUROS_HOME="/tmp/.sauros" && python3 install.py
